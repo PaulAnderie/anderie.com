8beb25a480aa8d0d085050da2110d6bc775915ed
PaulAnderie <p.anderie@prandible.com>
2019-05-23 14:05:45 +0200

    Live Push

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Paul Anderie LIVE" />
-    <h1>Hi 👋, ich bin Paul Anderie (GitHub)</h1>
+    <h1>Hi 👋, ich bin Paul Anderie LIVE</h1>
     <p>Ich bin der Gründer von <a href="https://prandible.com?utm_source=paul">prandible</a>, Hersteller von Software und Digital-Produkten und Technologie-Enthusiast.</p>
     <p> Meine Texte und Ideen konzentrieren sich auf die konträre Sichtweise, dass Geschäftswachstum nicht immer gut ist und nicht immer erforderlich ist.</p>
     <div style={{ maxWidth: `450px`, marginBottom: `1.45rem` }}>
